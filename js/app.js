625a479a9eee5ec4f0ad6dac910ccf876fa9b0a4
WillytheBot <markapostar@gmail.com>
2026-07-25 11:43:12 +0200

    feat: implementato frontend base simulatore robot (HTML, CSS, JS)

---
+++ js/app.js
@@ -0,0 +1,86 @@
+const stateElement = document.getElementById("robot-state");
+
+const xElement = document.getElementById("robot-x");
+const yElement = document.getElementById("robot-y");
+const zElement = document.getElementById("robot-z");
+
+const moveXPosButton = document.getElementById("move-x-pos");
+const moveYPosButton = document.getElementById("move-y-pos");
+const moveZPosButton = document.getElementById("move-z-pos");
+const moveXNegButton = document.getElementById("move-x-neg");
+const moveYNegButton = document.getElementById("move-y-neg");
+const moveZNegButton = document.getElementById("move-z-neg");
+
+const robot = {
+    name: "UR5e",
+    state: "IDLE",
+    position: { x: 0, y: 0, z: 0 },
+
+    startMovement() {
+        this.state = "MOVING";
+        updateUI();
+    },
+
+    finishMovement() {
+        setTimeout(() => {
+            this.state = "IDLE";
+            updateUI();
+        }, 500);
+    },
+
+    moveX(distance) {
+    if (this.state === "MOVING") {
+        return;
+    }
+    this.startMovement();
+    this.position.x += distance;
+    this.finishMovement();
+    },
+
+    moveY(distance) {
+    if (this.state === "MOVING") {
+        return;
+    }
+    this.startMovement();
+    this.position.y += distance;
+    this.finishMovement();
+    },
+    
+    moveZ(distance) {
+    if (this.state === "MOVING") {
+        return;
+    }
+    this.startMovement();
+    this.position.z += distance;
+    this.finishMovement();
+    }
+    
+}
+
+moveXPosButton.addEventListener("click", () => {
+    robot.moveX(1);
+});
+moveYPosButton.addEventListener("click", () => {
+    robot.moveY(1);
+});
+moveZPosButton.addEventListener("click", () => {
+    robot.moveZ(1);
+});
+moveXNegButton.addEventListener("click", () => {
+    robot.moveX(-1);
+});
+moveYNegButton.addEventListener("click", () => {
+    robot.moveY(-1);
+});
+moveZNegButton.addEventListener("click", () => {
+    robot.moveZ(-1);
+});
+
+function updateUI() {
+    stateElement.textContent = `State: ${robot.state}`;
+    xElement.textContent = `X: ${robot.position.x}`;
+    yElement.textContent = `Y: ${robot.position.y}`;
+    zElement.textContent = `Z: ${robot.position.z}`;
+}
+
+updateUI();
